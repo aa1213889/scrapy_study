8d39ea9fd0c74718125d10f149adc041f7656577
aa1213889 <869135421@qq.com>
2021-10-29 00:28:45 +0800

    0028

--- src/post/index.py
+++ src/post/index.py
@@ -1,5 +1,6 @@
 import urllib.request
 import urllib.parse
+import json
 
 url = 'https://fanyi.baidu.com/sug'
 
@@ -11,10 +12,15 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36'
 }
 
+# post请求参数必须要进行编码
+post_data = urllib.parse.urlencode(post_data).encode('utf-8')
+
 # urllib.request.Request data参数为post请求参数
 request = urllib.request.Request(url=url, data=post_data, headers=headers)
 
-# urlopen 可以传入一个Request的对象
+# 模拟浏览器向服务器发送请求
 response = urllib.request.urlopen(request)
+
+# 获取响应数据
 content = response.read().decode('utf-8')
-print(content)
+print(json.loads(content))
